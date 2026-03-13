4ef6ccbe7748a60f6e8a21e7a5a5a647f0047bbe
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2026-03-13 19:26:34 +0700

    feat: Inisialisasi struktur proyek Spring Boot

---
+++ src/main/java/com/example/ktpcrud/KtpcrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktpcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtpcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtpcrudApplication.class, args);
+	}
+
+}
